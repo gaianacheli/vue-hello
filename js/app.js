61ab261cefdb74351deecddb5e6547862d805cad
gaianacheli <gaianacheli04@gmail.com>
2021-10-28 15:05:22 +0200

    bonus: inserimento immagine

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var app = new Vue ({
     el:'#app',
     data: {
-        message: 'Ciao a tutti!'
+        message: 'Ciao a tutti!',
+        image: `https://picsum.photos/id/1005/600/200`
     },
 });
